c1df1c1f1a120e4d8b83d7ad8f3bf645651fb934
Siva Perumal K <sivaperumal644@gmail.com>
2019-10-09 20:36:31 +0530

    Announcement screen implemented.

--- lib/screens/announcement_screen.dart
+++ lib/screens/announcement_screen.dart
@@ -1,15 +1,39 @@
+import 'package:atlas/components/announcement_list_item.dart';
+import 'package:atlas/components/section_header.dart';
 import 'package:atlas/constants/colors.dart';
 import 'package:flutter/material.dart';
 
-class AnnouncementScreen extends StatelessWidget{
+class AnnouncementScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: WHITE_COLOR,
-      body: Container(
-        child: Text('2'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(height: 32),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            child: SectionHeader(
+              titleText: 'Announcements',
+              subTitleText: 'Updates will be posted here',
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: BouncingScrollPhysics(),
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: AnnouncementListItem(),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
